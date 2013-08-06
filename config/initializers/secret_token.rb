64092747357bc1b1f5099ca7fc7f824576228198
tim geaney <timgeaney24@gmail.com>
2013-08-06 13:29:31 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RafflerAngularjs::Application.config.secret_token = 'a3798428051f524d1bac17714516cca692b9a1befce7930ea8670bad1821fb72845247eb64a7a6044e72bf5102f3cf7d58fa9ea00567541e29691e0ed10fbd7b'
